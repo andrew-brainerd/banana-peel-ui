d6d9dc3d9190a2ec98392c75869db6251e5bf466
Andrew Brainerd <drwb333@gmail.com>
2020-09-01 17:39:55 -0400

    Add stage data to details

--- src/components/GameStats/GameStats.js
+++ src/components/GameStats/GameStats.js
@@ -1,27 +1,37 @@
 import React, { useEffect } from 'react';
-import { string, shape, array, func } from 'prop-types';
+import { string, shape, array, number, func } from 'prop-types';
 import isValidGameId from '../../utils/isValidGameId';
+import stageMap from '../../constants/stages';
+import Icon from '../common/Icon/Icon';
 import Metadata from './Metadata/container';
 import Actions from './Actions/Actions';
 import Overall from './Overall/Overall';
 import styles from './GameStats.module.scss';
 
-const GameStats = ({ gameId, stats, loadGame }) => {
+const GameStats = ({ gameId, stats, stageId, loadGame }) => {
   useEffect(() => {
     isValidGameId(gameId) && loadGame(gameId);
   }, [gameId, loadGame]);
 
   return stats ? (
-    <div className={styles.gameStats}>
-      <div className={styles.statContainer}>
-        <Metadata player={'Player 1'} />
-        <Overall player={stats.overall[0]} />
-        <Actions player={stats.actionCounts[0]} />
+    <div className={styles.game}>
+      <div className={styles.stageHeader}>
+        <div className={styles.stageName}>{(stageMap[stageId] || {}).name || stageId}</div>
       </div>
-      <div className={styles.statContainer}>
-        <Metadata player={'Player 2'} />
-        <Overall player={stats.overall[1]} />
-        <Actions player={stats.actionCounts[1]} />
+      <div className={styles.stageData}>
+        <Icon className={styles.stage} name={(stageMap[stageId] || {}).icon || 'default_stage'} />
+      </div>
+      <div className={styles.gameStats}>
+        <div className={styles.statContainer}>
+          <Metadata player={'Player 1'} />
+          <Overall player={stats.overall[0]} />
+          <Actions player={stats.actionCounts[0]} />
+        </div>
+        <div className={styles.statContainer}>
+          <Metadata player={'Player 2'} />
+          <Overall player={stats.overall[1]} />
+          <Actions player={stats.actionCounts[1]} />
+        </div>
       </div>
     </div>
   ) : null;
@@ -36,6 +46,7 @@ GameStats.propTypes = {
     overall: array,
     stocks: array
   }),
+  stageId: number,
   loadGame: func.isRequired
 };
 
